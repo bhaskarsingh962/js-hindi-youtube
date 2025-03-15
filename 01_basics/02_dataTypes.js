b5ffaa1b6e3142b39cd11a154b47585752283ee5
bhaskar singh <146477841+bhaskarsingh962@users.noreply.github.com>
2025-03-15 17:29:02 +0000

    this is just commit message

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict" //tret all js code as newer version
+
+//  alert("hello");  we are using node js not browser
+
+let  name = "bhasakr" // string data type
+let age = 13 //age
+let isLoggedin = false // boolean
+
+
+//premitive
+//number = 2 to the power 53
+//bigint 
+// string = ""
+//boolean = true/false;
+//null => standalone value;
+//undefined => when you dont assign any value;
+//symbol =>  unique
+
+//non primitive
+//object
+
+console.log(typeof undefined); //type in undefined
+console.log(typeof null); //type of null is object 
+
